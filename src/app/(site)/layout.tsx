430a297ccc4ea3fff73689530c272797c8bf5667
franck-co <franck@pedagome.fr>
2026-06-11 16:48:11 +0000

    clear template files

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,14 +1,7 @@
-import Footer from "../components/layout/footer"
-
 export default function SiteLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return (
-    <>
-      {children}
-      {/* <Footer /> */}
-    </>
-  )
+  return <>{children}</>
 }
